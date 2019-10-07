998993ef6e79678825e9354e33871a8e3ed9cf34
Marek Sebera <marek.sebera@gmail.com>
2019-10-07 22:40:12 +0200

    Extensive zoomable implementation

--- app/src/main/java/mareksebera/cz/redditswipe/fragments/ImageItemFragment.java
+++ app/src/main/java/mareksebera/cz/redditswipe/fragments/ImageItemFragment.java
@@ -1,15 +1,23 @@
 package mareksebera.cz.redditswipe.fragments;
 
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.drawable.ProgressBarDrawable;
+import com.facebook.drawee.drawable.ScalingUtils;
+import com.facebook.drawee.generic.GenericDraweeHierarchy;
+import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder;
+import com.facebook.drawee.interfaces.DraweeController;
 
 import mareksebera.cz.redditswipe.R;
+import mareksebera.cz.redditswipe.fresco.DefaultZoomableController;
+import mareksebera.cz.redditswipe.fresco.ZoomableController;
 import mareksebera.cz.redditswipe.fresco.ZoomableDraweeView;
 
 public class ImageItemFragment extends CommonItemFragment {
@@ -19,12 +27,23 @@ public class ImageItemFragment extends CommonItemFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = super.onCreateView(inflater, container, savedInstanceState);
 
+        assert v != null;
         ZoomableDraweeView drawee = v.findViewById(R.id.image_fragment_image);
         if (drawee != null) {
-            drawee.setController(Fresco.newDraweeControllerBuilder()
+            DraweeController controller = Fresco.newDraweeControllerBuilder()
+                    .setTapToRetryEnabled(true)
                     .setUri(item.DATA.getUrl())
                     .setAutoPlayAnimations(true)
-                    .build());
+                    .build();
+            GenericDraweeHierarchy hierarchy = new GenericDraweeHierarchyBuilder(getResources())
+                    .setActualImageScaleType(ScalingUtils.ScaleType.FIT_CENTER)
+                    .setProgressBarImage(new ProgressBarDrawable())
+                    .build();
+            ZoomableController zoomable = DefaultZoomableController.newInstance();
+
+            drawee.setController(controller);
+            drawee.setHierarchy(hierarchy);
+            drawee.setZoomableController(zoomable);
         }
 
         return v;

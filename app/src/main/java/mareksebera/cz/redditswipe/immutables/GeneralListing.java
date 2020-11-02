dde5e4d07239855c954257be0a772a7653651cd4
Marek Sebera <marek.sebera@gmail.com>
2020-11-02 11:07:31 +0100

    fixed

--- app/src/main/java/mareksebera/cz/redditswipe/immutables/GeneralListing.java
+++ app/src/main/java/mareksebera/cz/redditswipe/immutables/GeneralListing.java
@@ -14,21 +14,15 @@ import java.io.Serializable;
 @Value.Immutable
 @JsonDeserialize(as = ImmutableGeneralListing.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public interface GeneralListing extends GeneralData {
+public interface GeneralListing extends Serializable {
     @NonNull
     String getKind();
 
     @NonNull
-<<<<<<< HEAD
-    @JsonTypeInfo(use= JsonTypeInfo.Id.NAME,property="kind",include = JsonTypeInfo.As.EXTERNAL_PROPERTY)
-    @JsonSubTypes(value = {
-            @JsonSubTypes.Type(value = GeneralData.class, name="")
-=======
     @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "kind", include = JsonTypeInfo.As.EXTERNAL_PROPERTY, defaultImpl = Object.class)
     @JsonSubTypes(value = {
             @JsonSubTypes.Type(value = ImmutableGeneralData.class, name = "Listing"),
             @JsonSubTypes.Type(value = ImmutableGeneralListingItem.class, name = "t3")
->>>>>>> 3864457... stash
     })
     ImmutableGeneralData getData();
 
